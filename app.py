b9e580fb8ac283af3a7ed0b08417dd04f6307737
Ismail Sacic <cicas03@hotmail.com>
2021-10-04 22:19:52 +0200

    This is the one

--- app.py
+++ app.py
@@ -1,48 +1,17 @@
 #!/usr/bin/python
-from asyncio.tasks import sleep
-import sys, json
 import asyncio
-import websockets
-
-#Server address
-#ws://localhost:8000/gateway
-
-class Websocket():
-    async def __aenter__(self, uri:str = 'ws://localhost:8000/gateway'):
-        self._conn = websockets.connect( uri )
-        self.websocket = await self._conn.__aenter__()
-        return self
-
-    async def __aexit__(self, *args, **kwargs):
-        await self._conn.__aexit__(*args, **kwargs)
-
-    async def send(self, message):
-        await self.websocket.send(message)
-
-    async def receive(self):
-        return await self.websocket.recv()
-
-    async def close(self):
-        await self.websocket.close()
-
-async def send_message(server, json_message:dict, nonce:str):
-    json_message["nonce"] = nonce
-    async with server as echo:
-        await echo.send(json.dumps(json_message))
-        while True:
-            response = await echo.receive()
-            response = json.loads(response)
-            try:
-                if response["type"] != "auth" and response["nonce"] == nonce:
-                    return response
-            except Exception as e:
-                return {"error": e}
-
+from websocket_aaqc import Websocket
 
 async def main():
-    server = Websocket()
-    response = await send_message(server, {"type":"ping"}, nonce = "1200lol")
+    server = Websocket("wss://api.aaqc.svaren.dev/gateway")
+    
+    response = await server.send_message(server, {"type":"ping"}, nonce = "alve_svaren")
     print(response)
-        
+
+    while True:
+        pass
+
+
 if __name__ == "__main__":
     asyncio.run(main())
+    
---
+++ websocket_aaqc.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+from asyncio.tasks import sleep
+import sys, json, asyncio, websockets, json
+from time import sleep
+
+
+#Server address
+uri = "wss://api.aaqc.svaren.dev/gateway"
+
+class Websocket():
+    def __init__(self, uri):
+        self.uri = uri
+
+    async def listener(self):
+        print(await self.websocket.recv())
+    
+    async def __aenter__(self):
+        self._conn = websockets.connect( uri if uri != None else "wss://api.aaqc.svaren.dev/gateway")
+        self.websocket = await self._conn.__aenter__()
+        asyncio.ensure_future(self.listener())
+        return self
+
+    async def __aexit__(self, *args, **kwargs):
+        await self._conn.__aexit__(*args, **kwargs)
+
+    async def send(self, message):
+        await self.websocket.send(message)
+
+    async def receive(self):
+        return await self.websocket.recv()
+
+    async def close(self):
+        sleep(1)
+        await self._conn.__aexit__(*sys.exc_info())
+
+    async def send_message(self, server, json_message:dict, nonce:str):
+        json_message["nonce"] = nonce
+        async with server as echo:
+            await echo.send(json.dumps(json_message))
+            while True:
+                response = await echo.receive()
+                response = json.loads(response)
+                try:
+                    if response["type"] != "auth" and response["nonce"] == nonce:
+                        return response
+                except Exception as e:
+                    return {"error": e}
