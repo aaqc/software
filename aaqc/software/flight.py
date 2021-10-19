85fd2b9cf52aa4f12572846753513c25a244acd4
ismail424 <cicas03@hotmail.com>
2021-10-19 10:47:43 +0200

    Structure

---
+++ aaqc/software/flight.py
@@ -0,0 +1,19 @@
+class DroneFlight:
+    def __init__(self):
+        print("Init sensorer")
+
+    def begin(self):
+        print("Begin flight")
+        print("Open websocket")
+        print("Send camera footage")
+
+    def force_stop(self):
+        print("Force stop")
+        print("Stop motor")
+
+    def land(self):
+        print("Landing")
+
+    def end(self):
+        print("End flight")
+        print("")
